e73ad42677dfbae36261b63f47d3dc9eb366c870
Juna Dajang <junadajang@gmail.com>
2018-11-12 17:27:04 +0800

    I added login UI

---
+++ app/src/main/java/com/example/pc002/log_in/Dashboard.java
@@ -0,0 +1,15 @@
+package com.example.pc002.log_in;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+    }
+}
---
+++ app/src/main/java/com/example/pc002/log_in/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.pc002.log_in;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent in = new Intent(this, Dashboard.class);
+        startActivity(in);
+    }
+}
